330af75658213eadba4fa3df3ad923137a4ce94c
jakubbebacz <jakubbebacz0@gmail.com>
2022-01-30 21:21:17 +0100

    samesite error?

--- usos.API/Startup.cs
+++ usos.API/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Converters;
 using usos.API.Application.IServices;
